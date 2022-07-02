9dbc1f070c0ae5cd10ac024b3a06f7b39ddeed3c
bohyeon <ktseo41@gmail.com>
2022-07-02 21:01:58 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+alias g='git'
+alias co='checkout'
+alias ci='commit'
+alias lg='log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit'
